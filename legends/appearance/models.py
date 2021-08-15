6ffc321f423f136f018df077e50872c3cf6953a7
megamott <megamott27@gmail.com>
2021-08-15 10:04:00 +0300

    Add QuestionManager for custom queries.

--- legends/appearance/models.py
+++ legends/appearance/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
+class QuestionManager(models.Manager):
+    """ Manager for question model """
+
+    def get_queryset(self):
+        """ Override get_queryset method from BaseManager """
+        return super().get_queryset()
+
+    def find_by_complexity(self, complexity_level):
+        """ Find all questions by complexity: H, M, E """
+        return self.get_queryset().filter(complexity=complexity_level)
+
+    def find_by_group(self, group_name):
+        """
+        Find all questions by group
+        Example: find_by_group('Общие знания')
+        """
+        return self.get_queryset().filter(group=group_name)
+
+
 class Question(models.Model):
     """ Question for the quiz """
 
@@ -12,8 +31,8 @@ class Question(models.Model):
     )
 
     question_name = models.CharField(
-        verbose_name='question name, describing its essence',
-        max_length=200
+        max_length=200,
+        verbose_name='question name, describing its essence'
     )
     question_text = models.CharField(
         max_length=200
@@ -27,11 +46,17 @@ class Question(models.Model):
         max_length=1,
         choices=COMPLEXITY_TYPES
     )
+    group = models.CharField(
+        max_length=200,
+        default='Общие знания',
+        verbose_name='which group the question belongs to'
+    )
     hint = models.CharField(
-        verbose_name='a hint to a question to help answer it',
         max_length=200,
         null=True,
+        verbose_name='a hint to a question to help answer it'
     )
+    objects = QuestionManager()
 
     def __repr__(self):
         return self.question_text
