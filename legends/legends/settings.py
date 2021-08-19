bfe01ff50a2194390ba33bec25723b05fc9097ce
megamott <megamott27@gmail.com>
2021-08-19 22:22:23 +0300

    Refactor settings and gitignore file.

--- legends/legends/settings.py
+++ legends/legends/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'legends.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = (
+    (BASE_DIR / 'frontend/build/static'),
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
